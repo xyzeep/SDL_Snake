5711a5bf34e0bee34b947062d3d1026acd026886
pawanacharya73 <pawanacharya832@gmail.com>
2025-04-03 16:40:24 +0545

    update_segments

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
+#include <stdlib.h>
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 800
 #define COLOR_WHITE 0xffffffff
@@ -14,31 +15,50 @@
 #define COLS SCREEN_WIDTH/CELL_SIZE
 
 
-#define SNAKE(x, y) fill_cell(surface, x, y, COLOR_WHITE)
+#define SNAKE() draw_snake(surface, &snake)
 #define APPLE(x, y) fill_cell(surface, x, y, COLOR_APPLE)
+
+struct segment {
+    int index;
+    int x, y; //relative to cells
+};
+
 
 struct Snake {
     int x, y;
     int length; //relative to cell size
     double speed;
     int direction; //1, 2, 3, 4 (U, D, L, R)
+    struct segment* segments;
 };
 
 
+
+void update_segments(struct Snake* snake) {
+    // let's assume that the direction is right
+    for (int i = 0; i < snake->length - 1; i++) {
+        snake->segments[i] = snake->segments[i + 1];    
+    }
+
+    snake->segments[snake->length - 1].x ++;
+}
+
+void draw_snake(SDL_Surface* surface, struct Snake* snake) {
+    SDL_Rect rect = {0, 0, CELL_SIZE - GRID_WIDTH, CELL_SIZE-GRID_WIDTH};
+    for(int i = 0; i < snake->length; i++) {
+        rect.x = snake->segments[i].x * CELL_SIZE + GRID_WIDTH;
+        rect.y = snake->segments[i].y * CELL_SIZE + GRID_WIDTH;
+
+        SDL_FillSurfaceRect(surface, &rect, COLOR_WHITE);
+    }
+
+
+}
 
 void fill_cell(SDL_Surface* surface, int x, int y, Uint32 color) {
 
     SDL_Rect rect = {x * CELL_SIZE + GRID_WIDTH, y*CELL_SIZE+ GRID_WIDTH, CELL_SIZE - GRID_WIDTH, CELL_SIZE - GRID_WIDTH};
     SDL_FillSurfaceRect(surface, &rect, color);
-
-}
-
-void draw_snake (SDL_Surface* surface, struct Snake* snake) {
-    switch (snake->direction) {
-        case 1:
-            snake->y --;
-            break;
-    }
 
 }
 
@@ -64,15 +84,28 @@ int main() {
 
     SDL_Rect black_screen = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
     struct Snake snake  = {10, 10, 3, 1, 1};
-    int running = 1; //1 for true and 0 for false
-    SDL_Event event;
 
+    snake.segments = malloc(sizeof(struct segment) * 3);
+    if (snake.segments == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1); // Handl.e error appropriately
+    }
     //////////
     int snake_x = 10;
     int snake_y = 10;
     int apple_x = 18;
     int apple_y = 10;
     //////////
+
+    for (int i = 0; i < snake.length; i++) {
+        snake.segments[i].index = i;
+        snake.segments[i].x = snake_x + i;
+        snake.segments[i].y = snake_y;
+    }
+
+    int running = 1; //1 f r true and 0 for false
+    SDL_Event event;
+
 
     while(running) {
         while(SDL_PollEvent(&event)) {
@@ -103,19 +136,19 @@ int main() {
         //draw grids
         drawGrid(surface);
 
-        draw_snake(surface, &snake);
-        SNAKE(snake.x, snake.y);
-        APPLE(apple_x, apple_y);
+        update_segments(&snake);
+        SNAKE();
+        //APPLE(apple_x, apple_y);
 
         SDL_UpdateWindowSurface(window);
-        SDL_Delay(100);
+        SDL_Delay(1000);
     }
 
 
 
 
 
-
+    free(snake.segments);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
